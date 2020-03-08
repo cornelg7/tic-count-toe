9427d976ecc0e0f85d8412b9b2f2e5a85333f513
Cornel <cornelg7@yahoo.com>
2020-03-08 12:28:54 +0200

    Added build

---
+++ back-end/build/precache-manifest.c9536cb9a14ed90e8acaaecab667f4c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1a59dd9e639a61a433500651b6552fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04dede9d8afc7f15386b",
+    "url": "/static/css/main.edcd9362.chunk.css"
+  },
+  {
+    "revision": "b197b81185fab1dd008b",
+    "url": "/static/js/2.aa2024a0.chunk.js"
+  },
+  {
+    "revision": "1201c8656ff5734f581290e1df7d9b22",
+    "url": "/static/js/2.aa2024a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04dede9d8afc7f15386b",
+    "url": "/static/js/main.7170381e.chunk.js"
+  },
+  {
+    "revision": "691443989d219c585434",
+    "url": "/static/js/runtime-main.1eb18e34.js"
+  }
+]);
